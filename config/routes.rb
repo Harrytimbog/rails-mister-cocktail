6541c78a33f9c712a96167fe3a0a91b9f59106f3
arigbede emmanuel <Harriemannie@gmail.com>
2019-11-22 04:30:28 -0300

    add cocktail index, new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  post 'doses/create'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating cocktails'
+
+Cocktail.create(name: 'lemon')
+Cocktail.create(name: 'ice')
+Cocktail.create(name: 'mint leaves')
+
+puts 'Seed complete'
